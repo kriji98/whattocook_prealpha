be1f1eedd2aef526d000c1390d2c40e9fadbc562
kristiana <kriji98@abv.bg>
2016-05-11 09:57:00 +0300

    adding search.java

--- app/src/main/java/project/tp/whattocook/Search.java
+++ app/src/main/java/project/tp/whattocook/Search.java
@@ -1,14 +1,34 @@
 package project.tp.whattocook;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ListAdapter;
+import android.widget.SearchView;
 
-public class Search extends AppCompatActivity {
+import java.util.ArrayList;
+
+
+public class Search extends AppCompatActivity
+        implements SearchView.OnQueryTextListener, SearchView.OnCloseListener {
+
+    private SearchManager searchManager;
+    private android.widget.SearchView searchView;
+    private MyExpandableListAdapter listAdapter;
+    private ExpandableListView myList;
+    private ArrayList<ParentRow> parentList = new ArrayList<ParentRow>();
+    private ArrayList<ParentRow> showTheseParentList = new ArrayList<ParentRow>();
+    private MenuItem searchItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +44,78 @@ public class Search extends AppCompatActivity {
     //            Action_SearchToListOfRecipes(view);
             }
         });
+
+        searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        parentList = new ArrayList<ParentRow>();
+        showTheseParentList = new ArrayList<ParentRow>();
+
+        //The app will crash if display list is not called here.
+        displayList();
+
+        //This expands the list of ...
+        expandAll();
+    }
+
+    private void loadData(){
+        ArrayList<ChildRow> childRows = new ArrayList<ChildRow>();
+        ParentRow parentRow = null;
+
+        /*childRows.add(new ChildRow(R.mipmap.generic_icon, "hello"));
+        parentList.add(parentRow);*/
+    }
+
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++) {
+            myList.expandGroup(i);
+        }
+    }
+
+    private void displayList(){
+        loadData();
+
+        myList = (ExpandableListView) findViewById(R.id.expandableListView_search);
+        listAdapter = new MyExpandableListAdapter(Search.this, parentList);
+
+        myList.setAdapter(listAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setSearchableInfo
+                (searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(this);
+        searchView.setOnCloseListener(this);
+        searchView.requestFocus();
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onClose() {
+        listAdapter.filterData("");
+        expandAll();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        listAdapter.filterData(query);
+        expandAll();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        listAdapter.filterData(newText);
+        expandAll();
+        return false;
     }
 
     /*public void Action_SearchToListOfRecipes(View view)
